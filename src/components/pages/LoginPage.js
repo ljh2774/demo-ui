e35a688763be24c8fb17120398997fa82710c77c
LJH <ljh2976@gmail.com>
2021-02-28 16:25:07 +0900

    login

---
+++ src/components/pages/LoginPage.js
@@ -0,0 +1,54 @@
+import React, { useState } from 'react'
+import { useDispatch } from 'react-redux';
+import { loginUser } from '../../modules/action'
+import { withRouter } from 'react-router-dom';
+
+function LoginPage(props) {
+    const dispatch = useDispatch();
+
+    const [Email, setEmail] = useState("")
+    const [Password, setPassword] = useState("")
+
+    const onEmailHandler = (event) => {
+        setEmail(event.currentTarget.value)
+    }
+
+    const onPasswordHandler = (event) => {
+        setPassword(event.currentTarget.value)
+    }
+
+    const onSubmitHandler = (event) => {
+        event.preventDefault();
+
+        let body = {
+            email: Email,
+            password: Password
+        }
+
+        dispatch(loginUser(body))
+            .then((response) => {
+                
+                // if(response.payload.loginSuccess) {
+                //     props.history.push("/")
+                // } else {
+                //     alert('Login Error')
+                // }
+            })
+    }
+
+    return (
+        <div>
+            <form onSubmit={onSubmitHandler}>
+                <label>Email</label>
+                <input type="email" value={Email} onChange={onEmailHandler} />
+                <br />
+                <label>Password</label>
+                <input type="password" value={Password} onChange={onPasswordHandler} />
+                <br />
+                <button type="submit">Login</button>
+            </form>
+        </div>
+    )
+}
+
+export default withRouter(LoginPage)
